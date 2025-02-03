641bb3ac7b87839cb07110b9c34aea262296b448
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2025-02-03 07:16:53 +0000

    feat(json): Initial check-in of the `json` package

---
+++ packages/json/src/types.ts
@@ -0,0 +1,126 @@
+/*-------------------------------------------------------------------
+
+                       ⚡ Storm Software - Stryke
+
+ This code was released as part of the Stryke project. Stryke
+ is maintained by Storm Software under the Apache-2.0 License, and is
+ free for commercial and private use. For more information, please visit
+ our licensing page.
+
+ Website:         https://stormsoftware.com
+ Repository:      https://github.com/storm-software/stryke
+ Documentation:   https://stormsoftware.com/projects/stryke/docs
+ Contact:         https://stormsoftware.com/contact
+ License:         https://stormsoftware.com/projects/stryke/license
+
+ -------------------------------------------------------------------*/
+
+import type { ParseOptions } from "jsonc-parser";
+
+export type PrimitiveJsonValue = string | number | boolean | undefined | null;
+
+export type Class = {
+  new (...args: any[]): any;
+};
+
+export type JsonValue = PrimitiveJsonValue | JsonArray | JsonObject;
+
+export type JsonArray = Array<JsonValue>;
+
+export interface JsonObject {
+  [key: string]: JsonValue;
+}
+
+export type ClassInstance = any;
+
+export type SerializableJsonValue =
+  | symbol
+  | Set<JsonValue>
+  | Map<JsonValue, JsonValue>
+  | undefined
+  | bigint
+  | Date
+  | ClassInstance
+  | RegExp;
+
+export type Tree<T> = InnerNode<T> | Leaf<T>;
+export type Leaf<T> = [T];
+export type InnerNode<T> = [T, Record<string, Tree<T>>];
+
+export type PrimitiveTypeAnnotation = "number" | "undefined" | "bigint";
+export type LeafTypeAnnotation =
+  | PrimitiveTypeAnnotation
+  | "regexp"
+  | "Date"
+  | "Error"
+  | "URL";
+
+export type TypedArrayAnnotation = ["typed-array", string];
+export type ClassTypeAnnotation = ["class", string];
+export type SymbolTypeAnnotation = ["symbol", string];
+export type CustomTypeAnnotation = ["custom", string];
+export type SimpleTypeAnnotation = LeafTypeAnnotation | "map" | "set";
+export type CompositeTypeAnnotation =
+  | TypedArrayAnnotation
+  | ClassTypeAnnotation
+  | SymbolTypeAnnotation
+  | CustomTypeAnnotation;
+export type TypeAnnotation = SimpleTypeAnnotation | CompositeTypeAnnotation;
+
+export interface JsonParseOptions extends ParseOptions {
+  /**
+   * Expect JSON with javascript-style
+   *
+   * @defaultValue false
+   */
+  expectComments?: boolean;
+
+  /**
+   * Disallow javascript-style
+   *
+   * @defaultValue false
+   */
+  disallowComments?: boolean;
+
+  /**
+   * Allow trailing commas in the JSON content
+   */
+  allowTrailingComma?: boolean;
+}
+
+export interface JsonSerializeOptions {
+  /**
+   * the whitespaces to add as indentation to make the output more readable.
+   *
+   * @defaultValue 2
+   */
+  spaces?: number;
+}
+
+export interface JsonParserResult {
+  json: JsonValue;
+  meta?: {
+    values?:
+      | Tree<TypeAnnotation>
+      | Record<string, Tree<TypeAnnotation>>
+      | undefined;
+    referentialEqualities?:
+      | Record<string, string[]>
+      | [string[]]
+      | [string[], Record<string, string[]>];
+  };
+}
+
+export interface IJsonParser {
+  parse: <TData = any>(strData: string) => TData;
+  stringify: <TData = any>(data: TData) => string;
+  serialize: (object: JsonValue) => JsonParserResult;
+  deserialize: <TData = any>(payload: JsonParserResult) => TData;
+
+  register: <TData = any, TJsonValue extends JsonValue = JsonValue>(
+    name: string,
+    serialize: (object: JsonValue) => TJsonValue,
+    deserialize: (payload: TJsonValue) => TData,
+    isApplicable: (data: any) => data is TData
+  ) => void;
+}
---
+++ packages/json/src/utils/parse-error.ts
@@ -0,0 +1,53 @@
+/*-------------------------------------------------------------------
+
+                       ⚡ Storm Software - Stryke
+
+ This code was released as part of the Stryke project. Stryke
+ is maintained by Storm Software under the Apache-2.0 License, and is
+ free for commercial and private use. For more information, please visit
+ our licensing page.
+
+ Website:         https://stormsoftware.com
+ Repository:      https://github.com/storm-software/stryke
+ Documentation:   https://stormsoftware.com/projects/stryke/docs
+ Contact:         https://stormsoftware.com/contact
+ License:         https://stormsoftware.com/projects/stryke/license
+
+ -------------------------------------------------------------------*/
+
+import type { ParseError } from "jsonc-parser";
+import { printParseErrorCode } from "jsonc-parser";
+import { LinesAndColumns } from "lines-and-columns";
+import { codeFrameColumns } from "./code-frames";
+
+/**
+ * Nicely formats a JSON error with context
+ *
+ * @param input - JSON content as string
+ * @param parseError - jsonc ParseError
+ * @returns
+ */
+export function formatParseError(input: string, parseError: ParseError) {
+  const { error, offset, length } = parseError;
+  let result = new LinesAndColumns(input).locationForIndex(offset);
+  let line = result?.line ?? 0;
+  let column = result?.column ?? 0;
+
+  line++;
+  column++;
+
+  return (
+    `${printParseErrorCode(error)} in JSON at ${line}:${column}\n` +
+    codeFrameColumns(input, {
+      start: {
+        line,
+        column
+      },
+      end: {
+        line,
+        column: column + length
+      }
+    }) +
+    "\n"
+  );
+}
