0fd76b4497a7d494f8b3fcad85c3202649249d05
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2025-02-08 04:33:58 +0000

    fix(string-format): Fixed uncontrolled regular expression issue
    undefinedResolves [#6 Fix code scanning alert - Polynomial regular expression
    used on uncontrolled
    data](https://github.com/storm-software/stryke/issues/6)

--- packages/string-format/src/get-words.ts
+++ packages/string-format/src/get-words.ts
@@ -44,5 +44,11 @@ export const CASE_SPLIT_PATTERN = /[A-Z]?[a-z]+|\d+|[A-Z]+(?![a-z])/g;
  * @returns An array of words
  */
 export function getWords(str: string): string[] {
+  if (str.length > 1000) {
+    throw new Error(
+      "The regular expression parameter of `get-words` can't handle strings longer than 1000 characters"
+    );
+  }
+
   return [...(str.match(CASE_SPLIT_PATTERN) ?? [])];
 }
