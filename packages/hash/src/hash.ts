c6e4ff398a59333f44b7a4d0161aaf5f8f8256ea
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2025-02-26 06:20:10 +0000

    feat(hash): Added the `hashFolder` function

--- packages/hash/src/hash.ts
+++ packages/hash/src/hash.ts
@@ -1,4 +1,4 @@
-/*-------------------------------------------------------------------
+/* -------------------------------------------------------------------
 
                        ⚡ Storm Software - Stryke
 
@@ -13,10 +13,14 @@
  Contact:         https://stormsoftware.com/contact
  License:         https://stormsoftware.com/projects/stryke/license
 
- -------------------------------------------------------------------*/
+ ------------------------------------------------------------------- */
 
+import type { HashObjectOptions } from "./hash-object";
+import { statSync } from "node:fs";
+import { readdir, readFile } from "node:fs/promises";
+import { joinPaths } from "@stryke/path/join-path";
 import { isString } from "@stryke/types/type-checks/is-string";
-import { type HashObjectOptions, hashObject } from "./hash-object";
+import { hashObject } from "./hash-object";
 import { sha256base64 } from "./sha-256";
 
 export interface HashOptions extends HashObjectOptions {
@@ -37,9 +41,44 @@ export interface HashOptions extends HashObjectOptions {
  */
 export function hash(object: any, options?: HashOptions): string {
   const result = sha256base64(
-    isString(object) ? object : hashObject(object, options),
+    isString(object) ? object : hashObject(object, options)
   );
   const maxLength = options?.maxLength ?? 32;
 
   return result.length > maxLength ? result.slice(0, maxLength) : result;
+}
+
+/**
+ * Hash a folder path into a string based on the file content
+ *
+ * @param dir - The folder path to hash
+ * @param  options - Hashing options
+ * @returns A hashed string value
+ */
+export async function hashFolder(
+  dir: string,
+  options?: HashOptions
+): Promise<string> {
+  const files = [] as string[];
+  const getDirContent = async (dir: string) => {
+    const dirFiles = await readdir(dir);
+    dirFiles.forEach(file => {
+      const path = joinPaths(dir, file);
+      if (statSync(path).isDirectory()) {
+        getDirContent(path);
+      } else {
+        files.push(path);
+      }
+    });
+  };
+  await getDirContent(dir);
+
+  const result = {} as Record<string, string>;
+  await Promise.all(
+    files.map(async file => {
+      result[file] = await readFile(file, "utf8");
+    })
+  );
+
+  return hash(result, options);
 }
