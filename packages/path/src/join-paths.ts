14a3ef8c923f8245792569bf60e8aa0ebd1b5608
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2025-03-14 03:32:31 +0100

    fix(path): Resolve issue with `joinPaths` helper function

--- packages/path/src/join-paths.ts
+++ packages/path/src/join-paths.ts
@@ -15,19 +15,76 @@
 
  ------------------------------------------------------------------- */
 
+/* eslint-disable func-names */
+
 import type path from "node:path";
-import { correctPath } from "./correct-path";
+
+const _DRIVE_LETTER_START_RE = /^[A-Z]:\//i;
+
+// Util to normalize windows paths to posix
+function normalizeWindowsPath(input = "") {
+  if (!input) {
+    return input;
+  }
+  return input
+    .replace(/\\/g, "/")
+    .replace(_DRIVE_LETTER_START_RE, r => r.toUpperCase());
+}
+
+const _UNC_REGEX = /^[/\\]{2}/;
+const _IS_ABSOLUTE_RE = /^[/\\](?![/\\])|^[/\\]{2}(?!\.)|^[A-Z]:[/\\]/i;
+const _DRIVE_LETTER_RE = /^[A-Z]:$/i;
+
+const isAbsolute: typeof path.isAbsolute = function (p) {
+  return _IS_ABSOLUTE_RE.test(p);
+};
+
+const correctPaths = function (path?: string) {
+  if (!path || path.length === 0) {
+    return ".";
+  }
+
+  // Normalize windows argument
+  path = normalizeWindowsPath(path);
+
+  const isUNCPath = path.match(_UNC_REGEX);
+  const isPathAbsolute = isAbsolute(path);
+  const trailingSeparator = path[path.length - 1] === "/";
+
+  // Normalize the path
+  path = normalizeString(path, !isPathAbsolute);
+
+  if (path.length === 0) {
+    if (isPathAbsolute) {
+      return "/";
+    }
+    return trailingSeparator ? "./" : ".";
+  }
+  if (trailingSeparator) {
+    path += "/";
+  }
+  if (_DRIVE_LETTER_RE.test(path)) {
+    path += "/";
+  }
+
+  if (isUNCPath) {
+    if (!isPathAbsolute) {
+      return `//./${path}`;
+    }
+    return `//${path}`;
+  }
+
+  return isPathAbsolute && !isAbsolute(path) ? `/${path}` : path;
+};
 
 /**
- * Join multiple path segments together, resolving '.' and '..' segments, and normalizing the resulting path.
- *
- * @remarks
- * This helper utility performs string joining similar to the `node:path` module's `join` function, and then normalizes the resulting path.
+ * Joins all given path segments together using the platform-specific separator as a delimiter.
+ * The resulting path is normalized to remove any redundant or unnecessary segments.
  *
  * @param segments - The path segments to join.
- * @returns The joined path string.
+ * @returns The joined and normalized path string.
  */
-export const joinPaths: typeof path.join = (...segments) => {
+export const joinPaths: typeof path.join = function (...segments) {
   let path = "";
 
   for (const seg of segments) {
@@ -48,5 +105,80 @@ export const joinPaths: typeof path.join = (...segments) => {
     }
   }
 
-  return correctPath(path);
+  return correctPaths(path);
 };
+
+/**
+ * Resolves a string path, resolving '.' and '.' segments and allowing paths above the root.
+ *
+ * @param path - The path to normalize.
+ * @param allowAboveRoot - Whether to allow the resulting path to be above the root directory.
+ * @returns the normalized path string.
+ */
+function normalizeString(path: string, allowAboveRoot: boolean) {
+  let res = "";
+  let lastSegmentLength = 0;
+  let lastSlash = -1;
+  let dots = 0;
+  let char: string | null = null;
+  for (let index = 0; index <= path.length; ++index) {
+    if (index < path.length) {
+      // casted because we know it exists thanks to the length check
+      char = path[index] as string;
+    } else if (char === "/") {
+      break;
+    } else {
+      char = "/";
+    }
+    if (char === "/") {
+      if (lastSlash === index - 1 || dots === 1) {
+        // NOOP
+      } else if (dots === 2) {
+        if (
+          res.length < 2 ||
+          lastSegmentLength !== 2 ||
+          res[res.length - 1] !== "." ||
+          res[res.length - 2] !== "."
+        ) {
+          if (res.length > 2) {
+            const lastSlashIndex = res.lastIndexOf("/");
+            if (lastSlashIndex === -1) {
+              res = "";
+              lastSegmentLength = 0;
+            } else {
+              res = res.slice(0, lastSlashIndex);
+              lastSegmentLength = res.length - 1 - res.lastIndexOf("/");
+            }
+            lastSlash = index;
+            dots = 0;
+            continue;
+          } else if (res.length > 0) {
+            res = "";
+            lastSegmentLength = 0;
+            lastSlash = index;
+            dots = 0;
+            continue;
+          }
+        }
+        if (allowAboveRoot) {
+          res += res.length > 0 ? "/.." : "..";
+          lastSegmentLength = 2;
+        }
+      } else {
+        if (res.length > 0) {
+          res += `/${path.slice(lastSlash + 1, index)}`;
+        } else {
+          res = path.slice(lastSlash + 1, index);
+        }
+        lastSegmentLength = index - lastSlash - 1;
+      }
+      lastSlash = index;
+      dots = 0;
+    } else if (char === "." && dots !== -1) {
+      ++dots;
+    } else {
+      dots = -1;
+    }
+  }
+  return res;
+}
