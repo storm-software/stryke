e592eed57f835cb454b5c65ab56c022c506704b2
Pat <pat@patsullivan.org>
2025-03-28 06:52:38 +0000

    chore(prisma-trpc-generator): Update package to build with `tsup` script

---
+++ packages/prisma-trpc-generator/tsup.config.ts
@@ -0,0 +1,34 @@
+/* -------------------------------------------------------------------
+
+                       ⚡ Storm Software - Stryke
+
+ This code was released as part of the Stryke project. Stryke
+ is maintained by Storm Software under the Apache-2.0 License, and is
+ free for commercial and private use. For more information, please visit
+ our licensing page.
+
+ Website:         https://stormsoftware.com
+ Repository:      https://github.com/storm-software/stryke
+ Documentation:   https://stormsoftware.com/projects/stryke/docs
+ Contact:         https://stormsoftware.com/contact
+ License:         https://stormsoftware.com/projects/stryke/license
+
+ ------------------------------------------------------------------- */
+
+import { defineConfig } from "tsup";
+
+export default defineConfig([
+  {
+    name: "prisma-trpc-generator",
+    entryPoints: ["src/*.ts"],
+    format: ["cjs", "esm"],
+    outDir: "dist",
+    clean: true,
+    dts: false,
+    sourcemap: false,
+    tsconfig: "./tsconfig.json",
+    shims: true,
+    bundle: true,
+    noExternal: ["eslint-plugin-pnpm"]
+  }
+]);
