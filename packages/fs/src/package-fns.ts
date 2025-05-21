d73163d6bac9676a01f5d4929e55871aff05a49a
Pat <pat@patsullivan.org>
2025-05-21 00:24:50 +0000

    fix(fs): Resolve package detection issue when versions are provided

--- packages/fs/src/package-fns.ts
+++ packages/fs/src/package-fns.ts
@@ -3,15 +3,16 @@
                        ⚡ Storm Software - Stryke
 
  This code was released as part of the Stryke project. Stryke
- is maintained by Storm Software under the Apache-2.0 License, and is
+ is maintained by Storm Software under the Apache-2.0 license, and is
  free for commercial and private use. For more information, please visit
- our licensing page.
+ our licensing page at https://stormsoftware.com/projects/stryke/license.
 
- Website:         https://stormsoftware.com
- Repository:      https://github.com/storm-software/stryke
- Documentation:   https://stormsoftware.com/projects/stryke/docs
- Contact:         https://stormsoftware.com/contact
- License:         https://stormsoftware.com/projects/stryke/license
+ Website:                  https://stormsoftware.com
+ Repository:               https://github.com/storm-software/stryke
+ Documentation:            https://stormsoftware.com/projects/stryke/docs
+ Contact:                  https://stormsoftware.com/contact
+
+ SPDX-License-Identifier:  Apache-2.0
 
  ------------------------------------------------------------------- */
 
@@ -179,10 +180,18 @@ export async function loadPackageJSON(
  * @returns An indicator specifying if the package is listed
  */
 export async function isPackageListed(name: string, cwd?: string) {
+  let packageName = name;
+  if (packageName.includes("@")) {
+    packageName =
+      !packageName.startsWith("@") || packageName.lastIndexOf("@") > 0
+        ? packageName.slice(0, packageName.lastIndexOf("@"))
+        : packageName;
+  }
   const pkg = (await loadPackageJSON(cwd)) ?? {};
 
   return (
-    name in (pkg.dependencies ?? {}) || name in (pkg.devDependencies ?? {})
+    packageName in (pkg.dependencies ?? {}) ||
+    packageName in (pkg.devDependencies ?? {})
   );
 }
 
