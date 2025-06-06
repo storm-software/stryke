ae3b2e40f7542bf29bcdbd3eecd80c45e1ac7bfc
Pat <pat@patsullivan.org>
2025-06-06 18:07:41 +0000

    fix(fs): No longer throw errors in `removeFile`  when path does not exist

--- packages/fs/src/remove-file.ts
+++ packages/fs/src/remove-file.ts
@@ -23,11 +23,12 @@ import { rm } from "node:fs/promises";
  * Remove the given content to the given file path
  *
  * @param filePath - The file path to remove to
- * @param content - The content to remove to the file
  */
 export const removeFileSync = (filePath: string): void => {
   if (!filePath || !existsSync(filePath)) {
-    throw new Error("Invalid file path provided to remove data");
+    // eslint-disable-next-line no-console
+    console.warn("Skipping - Invalid file path provided to `removeFileSync`");
+    return;
   }
 
   rmSync(filePath);
@@ -37,11 +38,12 @@ export const removeFileSync = (filePath: string): void => {
  * Remove the given content to the given file path
  *
  * @param filePath - The file path to read to
- * @returns The content of the file
  */
 export const removeFile = async (filePath: string): Promise<void> => {
   if (!filePath || !existsSync(filePath)) {
-    throw new Error("Invalid file path provided to remove data");
+    // eslint-disable-next-line no-console
+    console.warn("Skipping - Invalid file path provided to `removeFile`");
+    return;
   }
 
   return rm(filePath);
