966160653a942828794c4acf88dfd2bd3556b308
Pat <pat@patsullivan.org>
2025-06-10 17:16:52 +0000

    fix(capnp): Resolve build issue by adding separate `vendor` build config

---
+++ packages/capnp/tsup.vendor.config.ts
@@ -0,0 +1,36 @@
+/* -------------------------------------------------------------------
+
+                       ⚡ Storm Software - Stryke
+
+ This code was released as part of the Stryke project. Stryke
+ is maintained by Storm Software under the Apache-2.0 license, and is
+ free for commercial and private use. For more information, please visit
+ our licensing page at https://stormsoftware.com/projects/stryke/license.
+
+ Website:                  https://stormsoftware.com
+ Repository:               https://github.com/storm-software/stryke
+ Documentation:            https://docs.stormsoftware.com/projects/stryke
+ Contact:                  https://stormsoftware.com/contact
+
+ SPDX-License-Identifier:  Apache-2.0
+
+ ------------------------------------------------------------------- */
+
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  name: "capnp-vendor",
+  entryPoints: ["src/vendor/_capnp/*.ts"],
+  format: ["cjs", "esm"],
+  platform: "node",
+  outDir: "dist/vendor",
+  clean: true,
+  dts: true,
+  cjsInterop: true,
+  sourcemap: false,
+  tsconfig: "./tsconfig.vendor.json",
+  shims: true,
+  bundle: true,
+  splitting: true,
+  noExternal: ["capnp-es"]
+});
