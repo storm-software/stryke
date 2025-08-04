8ba113cd572f733a96a75ea6c178332be3b65382
sullivanpj <pat@patsullivan.org>
2025-08-04 18:43:27 +0000

    fix(type-checks): Resolve type-check linting failures

--- packages/type-checks/src/is-typed.ts
+++ packages/type-checks/src/is-typed.ts
@@ -5,7 +5,7 @@
  This code was released as part of the Stryke project. Stryke
  is maintained by Storm Software under the Apache-2.0 license, and is
  free for commercial and private use. For more information, please visit
- our licensing page at https://stormsoftware.com/license.
+ our licensing page at https://stormsoftware.com/licenses/projects/stryke.
 
  Website:                  https://stormsoftware.com
  Repository:               https://github.com/storm-software/stryke
@@ -16,7 +16,7 @@
 
  ------------------------------------------------------------------- */
 
-import type { ITyped } from "@stryke/types/base";
+import type { Typed } from "@stryke/types/base";
 import { isObject } from "./is-object";
 import { isSet } from "./is-set";
 
@@ -26,7 +26,7 @@ import { isSet } from "./is-set";
  * @param value - The value to type check
  * @returns An indicator specifying if the value provided has a `__typename` property
  */
-export const isTyped = (value: unknown): value is ITyped => {
+export const isTyped = (value: unknown): value is Typed => {
   try {
     return isSet(value) && isObject(value) && "__typename" in value;
   } catch {
