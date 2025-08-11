e34b993bdd9a540b6184774beab35bb7ca69073d
sullivanpj <pat@patsullivan.org>
2025-08-11 07:40:52 +0000

    fix(capnp): Remove extra `createDirectory` from `capnp` command

--- packages/capnp/bin/capnpc.ts
+++ packages/capnp/bin/capnpc.ts
@@ -29,12 +29,9 @@ import {
   findWorkspaceRootSafe,
   handleProcess
 } from "@storm-software/config-tools/utilities";
-import { createDirectory } from "@stryke/fs/helpers";
 import { existsSync } from "@stryke/path/exists";
 import { findFilePath } from "@stryke/path/file-path-fns";
-import { getWorkspaceRoot } from "@stryke/path/get-workspace-root.js";
 import { joinPaths } from "@stryke/path/join-paths";
-import { replacePath } from "@stryke/path/replace.js";
 import { Command, Option } from "commander";
 import { writeFile } from "node:fs/promises";
 import { capnpc } from "../src/compile.js";
@@ -229,9 +226,9 @@ async function compileAction(options: CapnpcCLIOptions) {
         writeWarning(
           `Output directory "${findFilePath(options.output)}" does not exist, it will be created...`
         );
-        await createDirectory(
-          replacePath(findFilePath(options.output), getWorkspaceRoot())
-        );
+        // await createDirectory(
+        //   replacePath(findFilePath(options.output), getWorkspaceRoot())
+        // );
       }
     }
 
