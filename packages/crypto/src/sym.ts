39947e5df17db419a4ae773c7d98ac0f9112d1fa
sullivanpj <pat@patsullivan.org>
2025-09-08 09:49:39 +0000

    feat(crypto): Added the `sym` module for symmetric ChaCha20-Poly1305 encryption

--- packages/crypto/src/sym.ts
+++ packages/crypto/src/sym.ts
@@ -16,13 +16,21 @@
 
  ------------------------------------------------------------------- */
 
+import { isSetString } from "@stryke/type-checks/is-set-string";
 import { Buffer } from "node:buffer";
-import crypto from "node:crypto";
+import type { BinaryLike, KeyObject } from "node:crypto";
+import {
+  createCipheriv,
+  createDecipheriv,
+  createSecretKey,
+  pbkdf2Sync,
+  randomBytes
+} from "node:crypto";
 
 // Background:
 // https://security.stackexchange.com/questions/184305/why-would-i-ever-use-aes-256-cbc-if-aes-256-gcm-is-more-secure
 
-const CIPHER_ALGORITHM = `aes-256-gcm`;
+const CIPHER_ALGORITHM = "chacha20-poly1305"; // `aes-256-gcm`;
 const CIPHER_KEY_LENGTH = 32; // https://stackoverflow.com/a/28307668/4397028
 const CIPHER_IV_LENGTH = 16; // https://stackoverflow.com/a/28307668/4397028
 const CIPHER_TAG_LENGTH = 16;
@@ -31,18 +39,51 @@ const CIPHER_SALT_LENGTH = 64;
 const PBKDF2_ITERATIONS = 100_000; // https://support.1password.com/pbkdf2/
 
 /**
+ * Creates and returns a new key object containing a secret key for symmetric encryption or `Hmac`.
+ *
+ * @param key - The key to use when creating the `KeyObject`.
+ * @returns The new `KeyObject`.
+ */
+export function createSecret(key: NodeJS.ArrayBufferView): KeyObject;
+
+/**
+ * Creates and returns a new key object containing a secret key for symmetric encryption or `Hmac`.
+ *
+ * @param key - The key to use. If `key` is a `Buffer`, `TypedArray`, or `DataView`, the `encoding` argument is ignored.
+ * @param encoding - The `encoding` of the `key` string. Must be one of `'utf8'`, `'utf16le'`, `'latin1'`, or `'base64'`. Default is `'utf8'`.
+ * @returns The new `KeyObject`.
+ */
+export function createSecret(key: string, encoding: BufferEncoding): KeyObject;
+
+/**
+ * Creates and returns a new key object containing a secret key for symmetric encryption or `Hmac`.
+ *
+ * @param key - The key to use. If `key` is a `Buffer`, `TypedArray`, or `DataView`, the `encoding` argument is ignored.
+ * @param encoding - The `encoding` of the `key` string. Must be one of `'utf8'`, `'utf16le'`, `'latin1'`, or `'base64'`. Default is `'utf8'`.
+ * @returns The new `KeyObject`.
+ */
+export function createSecret(
+  key: string | NodeJS.ArrayBufferView,
+  encoding?: BufferEncoding
+): KeyObject {
+  return isSetString(key)
+    ? createSecretKey(key, encoding!)
+    : createSecretKey(key);
+}
+
+/**
  * Encrypts data using a secret.
  *
  * @param secret - The secret key used for encryption.
  * @param data - The data to encrypt.
  * @returns The encrypted data.
  */
-export function encryptWithSecret(secret: Buffer, data: string): string {
-  const iv = crypto.randomBytes(CIPHER_IV_LENGTH);
-  const salt = crypto.randomBytes(CIPHER_SALT_LENGTH);
+export function encryptWithSecret(secret: BinaryLike, data: string): string {
+  const iv = randomBytes(CIPHER_IV_LENGTH);
+  const salt = randomBytes(CIPHER_SALT_LENGTH);
 
   // https://nodejs.org/api/crypto.html#crypto_crypto_pbkdf2sync_password_salt_iterations_keylen_digest
-  const key = crypto.pbkdf2Sync(
+  const key = pbkdf2Sync(
     secret,
     salt,
     PBKDF2_ITERATIONS,
@@ -50,9 +91,9 @@ export function encryptWithSecret(secret: Buffer, data: string): string {
     `sha512`
   );
 
-  const cipher = crypto.createCipheriv(CIPHER_ALGORITHM, key, iv);
+  const cipher = createCipheriv(CIPHER_ALGORITHM, key, iv);
   const encrypted = Buffer.concat([
-    cipher.update(data, `utf8`),
+    cipher.update(data, "utf8"),
     cipher.final()
   ]);
 
@@ -60,15 +101,12 @@ export function encryptWithSecret(secret: Buffer, data: string): string {
   const tag = cipher.getAuthTag();
 
   return Buffer.concat([
-    // Data as required by:
-    // Salt for Key: https://nodejs.org/api/crypto.html#crypto_crypto_pbkdf2sync_password_salt_iterations_keylen_digest
-    // IV: https://nodejs.org/api/crypto.html#crypto_class_decipher
-    // Tag: https://nodejs.org/api/crypto.html#crypto_decipher_setauthtag_buffer
-    salt,
-    iv,
-    tag,
+    // Data as required by: https://nodejs.org/api/crypto.html#crypto_crypto_createcipheriv_algorithm_key_iv_options
+    salt, // Salt for Key: https://nodejs.org/api/crypto.html#crypto_crypto_pbkdf2sync_password_salt_iterations_keylen_digest
+    iv, // IV: https://nodejs.org/api/crypto.html#crypto_class_decipher
+    tag, // Tag: https://nodejs.org/api/crypto.html#crypto_decipher_setauthtag_buffer
     encrypted
-  ]).toString(`hex`);
+  ]).toString("hex");
 }
 
 /**
@@ -79,10 +117,10 @@ export function encryptWithSecret(secret: Buffer, data: string): string {
  * @returns The decrypted data.
  */
 export function decryptWithSecret(
-  secret: Buffer,
+  secret: BinaryLike,
   encryptedData: string
 ): string {
-  const buffer = Buffer.from(encryptedData, `hex`);
+  const buffer = Buffer.from(encryptedData, "hex");
 
   const salt = buffer.slice(0, CIPHER_SALT_LENGTH);
   const iv = buffer.slice(
@@ -98,7 +136,7 @@ export function decryptWithSecret(
   );
 
   // https://nodejs.org/api/crypto.html#crypto_crypto_pbkdf2sync_password_salt_iterations_keylen_digest
-  const key = crypto.pbkdf2Sync(
+  const key = pbkdf2Sync(
     secret,
     salt,
     PBKDF2_ITERATIONS,
@@ -106,9 +144,9 @@ export function decryptWithSecret(
     `sha512`
   );
 
-  const decipher = crypto.createDecipheriv(CIPHER_ALGORITHM, key, iv);
+  const decipher = createDecipheriv(CIPHER_ALGORITHM, key, iv);
   decipher.setAuthTag(tag);
 
   // eslint-disable-next-line ts/restrict-plus-operands
-  return decipher.update(encrypted) + decipher.final(`utf8`);
+  return decipher.update(encrypted) + decipher.final("utf8");
 }
