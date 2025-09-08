90c46a2a929b1c3dfe9e1843db38b6057d49f549
sullivanpj <pat@patsullivan.org>
2025-09-08 18:02:14 +0000

    fix(prisma-better-auth-generator): Resolved issues with out of date package references

--- packages/prisma-better-auth-generator/src/utils/get-jiti.ts
+++ packages/prisma-better-auth-generator/src/utils/get-jiti.ts
@@ -3,23 +3,24 @@
                        ⚡ Storm Software - Stryke
 
  This code was released as part of the Stryke project. Stryke
- is maintained by Storm Software under the Apache-2.0 License, and is
+ is maintained by Storm Software under the Apache-2.0 license, and is
  free for commercial and private use. For more information, please visit
- our licensing page.
+ our licensing page at https://stormsoftware.com/licenses/projects/stryke.
 
- Website:         https://stormsoftware.com
- Repository:      https://github.com/storm-software/stryke
- Documentation:   https://stormsoftware.com/projects/stryke/docs
- Contact:         https://stormsoftware.com/contact
- License:         https://stormsoftware.com/projects/stryke/license
+ Website:                  https://stormsoftware.com
+ Repository:               https://github.com/storm-software/stryke
+ Documentation:            https://docs.stormsoftware.com/projects/stryke
+ Contact:                  https://stormsoftware.com/contact
+
+ SPDX-License-Identifier:  Apache-2.0
 
  ------------------------------------------------------------------- */
 
-import { getEnvPaths } from "@stryke/env/get-env-paths";
-import { getWorkspaceRoot } from "@stryke/path/get-workspace-root";
-import { joinPaths } from "@stryke/path/join-paths";
 import type { Jiti } from "jiti";
 import { createJiti } from "jiti";
+import { getEnvPaths } from "../../../env/src/get-env-paths";
+import { getWorkspaceRoot } from "../../../fs/src/get-workspace-root";
+import { joinPaths } from "../../../path/src/join-paths";
 
 let jiti!: Jiti;
 
