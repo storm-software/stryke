3c8d4601fe4ec21f7270f5c6790c52e66cc6d222
sullivanpj <pat@patsullivan.org>
2025-09-11 21:25:44 +0000

    feat(fs): Added the `relativeToWorkspaceRoot` and `relativeToProjectRoot` helpers

--- packages/fs/src/get-workspace-root.ts
+++ packages/fs/src/get-workspace-root.ts
@@ -18,6 +18,7 @@
 
 import { findWorkspaceRootSafe } from "@storm-software/config-tools";
 import { cwd } from "@stryke/path/cwd";
+import { relativePath } from "@stryke/path/file-path-fns";
 import { getParentPath } from "@stryke/path/get-parent-path";
 import { isSystemRoot } from "@stryke/path/is-root-dir";
 
@@ -61,7 +62,7 @@ export const PROJECT_ROOT_CONTENT: string[] = [
  * @param dir - A directory to start the search from
  * @returns The workspace root path
  */
-export function getWorkspaceRoot(dir = process.cwd()) {
+export function getWorkspaceRoot(dir = cwd()) {
   if (process.env.STORM_WORKSPACE_ROOT || process.env.NX_WORKSPACE_ROOT_PATH) {
     return (process.env.STORM_WORKSPACE_ROOT ||
       process.env.NX_WORKSPACE_ROOT_PATH)!;
@@ -94,7 +95,7 @@ export function getWorkspaceRoot(dir = process.cwd()) {
  * @param dir - A directory to check
  * @returns True if the directory is the workspace root, false otherwise
  */
-export function isWorkspaceRoot(dir: string = cwd()): boolean {
+export function isWorkspaceRoot(dir = cwd()): boolean {
   const workspaceRoot = getWorkspaceRoot(dir);
   if (workspaceRoot) {
     return workspaceRoot === dir;
@@ -109,7 +110,7 @@ export function isWorkspaceRoot(dir: string = cwd()): boolean {
  * @param dir - A directory to start the search from
  * @returns The project root path
  */
-export function getProjectRoot(dir = process.cwd()) {
+export function getProjectRoot(dir = cwd()) {
   const result = getParentPath(PROJECT_ROOT_CONTENT, dir);
 
   if (result) {
@@ -125,11 +126,31 @@ export function getProjectRoot(dir = process.cwd()) {
  * @param dir - A directory to check
  * @returns True if the directory is the project root, false otherwise
  */
-export function isProjectRoot(dir: string = process.cwd()): boolean {
+export function isProjectRoot(dir = cwd()): boolean {
   const projectRoot = getProjectRoot(dir);
   if (projectRoot) {
     return projectRoot === dir;
   }
 
   return false;
+}
+
+/**
+ * Find the file path relative to the workspace root path.
+ *
+ * @param filePath - The file path to process
+ * @returns The file path relative to the workspace root
+ */
+export function relativeToWorkspaceRoot(filePath: string) {
+  return relativePath(filePath, getWorkspaceRoot());
+}
+
+/**
+ * Find the file path relative to the project root path.
+ *
+ * @param filePath - The file path to process
+ * @returns The file path relative to the project root
+ */
+export function relativeToProjectRoot(filePath: string) {
+  return relativePath(filePath, getProjectRoot());
 }
