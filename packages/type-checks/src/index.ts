3b628bac191b5f97d75d10de357cdd4f85a36941
sullivanpj <pat@patsullivan.org>
2025-11-30 12:26:09 +0000

    feat(type-checks): Added `isURL` utility function

--- packages/type-checks/src/index.ts
+++ packages/type-checks/src/index.ts
@@ -5,7 +5,7 @@
  This code was released as part of the Stryke project. Stryke
  is maintained by Storm Software under the Apache-2.0 license, and is
  free for commercial and private use. For more information, please visit
- our licensing page at https://stormsoftware.com/license.
+ our licensing page at https://stormsoftware.com/licenses/projects/stryke.
 
  Website:                  https://stormsoftware.com
  Repository:               https://github.com/storm-software/stryke
@@ -64,5 +64,6 @@ export * from "./is-symbol";
 export * from "./is-typed";
 export * from "./is-typed-array";
 export * from "./is-undefined";
+export * from "./is-url";
 export * from "./property-exists";
 export * from "./type-detect";
--- packages/type-checks/src/is-url.ts
+++ packages/type-checks/src/is-url.ts
@@ -16,6 +16,23 @@
 
  ------------------------------------------------------------------- */
 
-export const isUndefined = (value: unknown): value is undefined => {
-  return value === undefined;
+import { isSetObject } from "./is-set-object";
+import { isSetString } from "./is-set-string";
+
+/**
+ * Determine if the provided value is of type URL
+ *
+ * @param value - The value to type check
+ * @returns An indicator specifying if the value provided is of type `URL`
+ */
+export const isURL = (value: unknown): value is URL => {
+  return (
+    isSetObject(value) &&
+    isSetString((value as URL).hash) &&
+    isSetString((value as URL).host) &&
+    isSetString((value as URL).hostname) &&
+    isSetString((value as URL).href) &&
+    isSetString((value as URL).origin) &&
+    isSetString((value as URL).protocol)
+  );
 };
