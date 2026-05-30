31d85e196d80aecaa13b02520f6d05dcfaa09249
sullivanpj <pat@patsullivan.org>
2026-05-30 20:31:22 +0000

    fix(monorepo): Resolve issue with `.test.ts` exports in `package.json` file

--- tools/config/tsdown.config.ts
+++ tools/config/tsdown.config.ts
@@ -16,7 +16,7 @@
 
  ------------------------------------------------------------------- */
 
-import type { UserConfig } from "tsdown";
+import type { TsdownInputOption, UserConfig } from "tsdown";
 import { defineConfig } from "tsdown";
 
 export type TSDownOptions = Partial<UserConfig> & Pick<UserConfig, "name">;
@@ -27,9 +27,7 @@ export const DEFAULT_OPTIONS: Omit<Partial<UserConfig>, "name"> = {
   format: ["cjs", "esm"],
   cjsDefault: true,
   treeshake: true,
-  exports: {
-    all: true
-  },
+  exports: true,
   clean: false,
   sourcemap: false,
   platform: "node",
@@ -45,7 +43,71 @@ export const DEFAULT_OPTIONS: Omit<Partial<UserConfig>, "name"> = {
   }
 };
 
-export default DEFAULT_OPTIONS;
+function getNegativeTestPath(path: string) {
+  if (
+    !path.startsWith("!") &&
+    !path.endsWith(".test.ts") &&
+    !path.endsWith(".test.tsx") &&
+    !path.endsWith(".test.{ts,tsx}")
+  ) {
+    if (path.endsWith(".ts")) {
+      return `!${path.replace(/\.ts$/, ".test.ts")}`;
+    } else if (path.endsWith(".tsx")) {
+      return `!${path.replace(/\.tsx$/, ".test.tsx")}`;
+    } else if (path.endsWith(".{ts,tsx}")) {
+      return `!${path.replace(/\.\{ts,tsx\}$/, ".test.{ts,tsx}")}`;
+    }
+  }
+
+  return undefined;
+}
+
+function updateEntry(entry: TsdownInputOption) {
+  return Array.isArray(entry) || typeof entry === "string"
+    ? (Array.isArray(entry) ? entry : [entry]).reduce(
+        (acc, path) => {
+          if (typeof path === "string") {
+            acc.push(path);
+
+            const testPath = getNegativeTestPath(path);
+            if (testPath) {
+              acc.push(testPath);
+            }
+          } else if (typeof path === "object") {
+            acc.push(updateEntry(path) as Record<string, string | string[]>);
+          }
+
+          return acc;
+        },
+        [] as (string | Record<string, string | string[]>)[]
+      )
+    : Object.entries(entry).reduce(
+        (acc, [key, value]) => {
+          if (typeof value === "string") {
+            acc[key] = [value];
+
+            const testPath = getNegativeTestPath(value);
+            if (testPath) {
+              acc[key].push(testPath);
+            }
+          } else if (Array.isArray(value)) {
+            acc[key] = value.reduce((acc, path) => {
+              acc.push(path);
+
+              const testPath = getNegativeTestPath(path);
+              if (testPath) {
+                acc.push(testPath);
+              }
+
+              return acc;
+            }, [] as string[]);
+          }
+
+          return acc;
+        },
+        {} as Record<string, string | string[]>
+      );
+}
 
 export function defineTSDownConfig(options: TSDownOptions | TSDownOptions[]) {
   return Array.isArray(options)
@@ -55,7 +117,8 @@ export function defineTSDownConfig(options: TSDownOptions | TSDownOptions[]) {
           onSuccess: async () => {
             console.log(` ✔ ${option.name} build completed successfully!`);
           },
-          ...option
+          ...option,
+          entry: updateEntry(option.entry ?? "!src/*.test.{ts,tsx}")
         }))
       )
     : defineConfig({
@@ -63,6 +126,9 @@ export function defineTSDownConfig(options: TSDownOptions | TSDownOptions[]) {
         onSuccess: async () => {
           console.log(` ✔ ${options.name} build completed successfully!`);
         },
-        ...options
+        ...options,
+        entry: updateEntry(options.entry ?? "!src/*.test.{ts,tsx}")
       });
 }
+
+export default defineTSDownConfig(DEFAULT_OPTIONS);
